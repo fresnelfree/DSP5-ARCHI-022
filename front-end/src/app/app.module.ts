37101c83599b2dbe62440cd56ba8e5600ffd6e7d
Issa-barry <issabarry67@gmail.com>
2023-07-28 19:25:15 +0200

    Angular material installé

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
- 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
 
   ],
   providers: [],
