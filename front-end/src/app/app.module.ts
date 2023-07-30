bd730151a5a0421f80afe9d96deba513493abb71
Issa-barry <issabarry67@gmail.com>
2023-07-30 15:02:33 +0200

    maj

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    
 
   ],
   providers: [],
