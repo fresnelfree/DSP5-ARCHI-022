e2e7c171a3bffb2ae8a10eee5f9baf3138d6c37d
Issa-barry <issabarry67@gmail.com>
2023-09-10 01:25:07 +0200

    Maj client all + mise en place html edit

--- front-end/src/app/dashboards/dashboards-routing.module.ts
+++ front-end/src/app/dashboards/dashboards-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ClientListComponent } from './clients/client-list/client-list.component';
+import { ClientEditComponent } from './clients/client-edit/client-edit.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'client', component: ClientListComponent }
+  // { path: 'client', children: [
+  //   { path: 'all', component: ClientListComponent },
+  // ] },
+
+  {
+    path: 'client', children: [
+      { path : '', component: ClientListComponent},
+      { path : 'all', component: ClientListComponent},
+      { path : 'edit', component: ClientEditComponent}
+    ]
+  }
 
 ];
 
