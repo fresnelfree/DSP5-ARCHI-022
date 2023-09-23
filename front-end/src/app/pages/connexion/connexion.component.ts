4c5bc0044115dc3c22a226f1bfac7fba5b50e31c
Issa-barry <issabarry67@gmail.com>
2023-09-23 14:49:30 +0200

    maj

--- front-end/src/app/pages/connexion/connexion.component.ts
+++ front-end/src/app/pages/connexion/connexion.component.ts
@@ -94,9 +94,9 @@ export class ConnexionComponent {
    
     this.authService.changeAuthStatus(true);
 
-    // this.router.navigate(['/dashboard']).then(() => {
-    //   window.location.reload();
-    // });
+    this.router.navigate(['/dashboard']).then(() => {
+      window.location.reload();
+    });
      
   }
 
