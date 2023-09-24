967f45c8f99aa0142328224b51fa1a4b4b5a459f
Issa-barry <issabarry67@gmail.com>
2023-09-24 19:27:39 +0200

    Da-172: creation du component "header"

--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
  
  
  
@@ -7,6 +8,8 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
      
+  
+    HeaderComponent
   ],
   imports: [
     CommonModule,
