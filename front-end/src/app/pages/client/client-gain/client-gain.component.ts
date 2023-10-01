d03290db92ff655af438879604759f55b7e07b67
Issa-barry <issabarry67@gmail.com>
2023-10-01 21:47:54 +0200

    DA-172  MISE EN PLACE DE GAIN CLIENT

--- front-end/src/app/pages/client/client-gain/client-gain.component.ts
+++ front-end/src/app/pages/client/client-gain/client-gain.component.ts
@@ -2,10 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { isNull } from 'cypress/types/lodash';
+import { Gain } from 'src/app/core/models/gain/gain';
 import { User } from 'src/app/core/models/user/user';
 import { AuthenticationService } from 'src/app/core/services/auth/authentication.service';
+import { GainService } from 'src/app/core/services/gain/gain.service';
 import { TokenService } from 'src/app/core/services/token/token.service';
-import { UserService } from 'src/app/core/services/user/user.service';
+ 
 
 
 @Component({
@@ -13,36 +16,55 @@ import { UserService } from 'src/app/core/services/user/user.service';
   templateUrl: './client-gain.component.html',
   styleUrls: ['./client-gain.component.css']
 })
-export class ClientGainComponent {
+export class ClientGainComponent implements OnInit {
   
   public user: any;
+  public gain: Gain;
+  public message: string;
 
   constructor(
     private router               : Router,
     private fb                   : FormBuilder ,
     private activatedRoute       : ActivatedRoute,
-    private userService          : UserService,
+    private gainService          : GainService,
     private token                : TokenService,
-    private authService          : AuthenticationService){}
+    private authService          : AuthenticationService){
+      this.message = "";
+      this.gain = new Gain()
+    }
 
-    ngOnInit(): void {
-      // this.getUser();
-      this.getUserByEmail()
-  }
+ 
+  
 
+  getGain(){
 
-    
-  getUserByEmail():  void{
+    return this.gainService.getUserByEmail(this.getTokenEmail()).subscribe(
+      (res) => { 
+        this.user = res //on stock le resultat dans la var user, afin de recuperer l'id
 
-    this.userService.getUserByEmail(this.getTokenEmail()).subscribe(
-      (res) => { this.user = res }
+        this.gainService.getGain(this.user.client.id).subscribe(
+          (res) => {
+            this.gain = res   
+          }
+        )
+        
+      }
     )
   }
 
-
   getTokenEmail() {
-    return this.userService.getTokenEmail();  
+    return this.gainService.getTokenEmail();  
   }
 
 
+  handleGain(){
+    if(this.gain.id === 0){
+    return  this.message = "Vous n'avez pas de ticket pour le moment."
+    }else return false
+  }
+ 
+    ngOnInit(): void {
+      this.getGain()
+      this.handleGain()
+  }
 }
