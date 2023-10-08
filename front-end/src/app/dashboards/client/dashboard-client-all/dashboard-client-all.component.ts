7bb30a46f282adaa78ad05f867459b1859a93bfc
Issa-barry <issabarry67@gmail.com>
2023-10-08 21:18:23 +0200

    DA-172 maj dashboard home

--- front-end/src/app/dashboards/client/dashboard-client-all/dashboard-client-all.component.ts
+++ front-end/src/app/dashboards/client/dashboard-client-all/dashboard-client-all.component.ts
@@ -4,78 +4,36 @@ import { ToggleService } from 'src/app/core/services/toggle/toggle.service';
 @Component({
   selector: 'app-dashboard-client-all',
   templateUrl: './dashboard-client-all.component.html',
-  styleUrls: ['./dashboard-client-all.component.css']
+  styleUrls: ['./dashboard-client-all.component.css'],
 })
 export class DashboardClientAllComponent {
+  public open: boolean = false;
+  public openMenu: boolean = false;
+  public openMenuSmall: boolean = false;
+  public ecran: number = window.innerWidth;
+  public smallDevise: boolean = false;
+  public block: boolean = false;
 
-//   public openMenu : boolean = false;
-//   public openMenuMobile : boolean = false;
-//   public openMenuTablette : boolean = false;
-
-
-
-public open: boolean = false
-public openMenu: boolean = false
-  public openMenuSmall : boolean = false;
-public ecran : number = window.innerWidth;
-public smallDevise: boolean = false
-public block: boolean = false
-  
-//   constructor(private toggleService: ToggleService){}
-
-//  ngOnInit(): void {
-     
-//  }
-
-isClicked = false;
+  //   constructor(private toggleService: ToggleService){}
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
     this.ecran = window.innerWidth;
 
-    if(this.ecran < 1010){
-      this.smallDevise = !this.smallDevise
+    if (this.ecran < 1010) {
+      this.smallDevise = !this.smallDevise;
     }
-   
-   }
+  }
 
-   
-//   onBars(e:MouseEvent){
-//     e.preventDefault()
-    
-//     if(this.ecran > 0 && this.ecran <=576)
-//     {
-//       this.openMenuMobile = !this.openMenuMobile
-//       this.openMenuSmall = false; 
-//     } 
-//     else if(this.ecran > 576 && this.ecran <778)
-//     {
-//       this.openMenuTablette = !this.openMenuTablette; 
-//       this.openMenuSmall = false; 
-//     }
-//     else if(this.ecran > 778)
-//     {
-//         this.openMenuSmall = !this.openMenuSmall;     
-//     }
-    
-//   }
+  onMenu(e: MouseEvent) {
+    if (this.ecran > 1010) {
+      this.open = !this.open;
+    } else if (this.ecran < 1010) {
+      this.openMenu = !this.openMenu;
 
-
-
- onMenu(e: MouseEvent){
-   
-        if(this.ecran>1010){
-          this.open = !this.open
-      } else if(this.ecran<1010){
-          this.openMenu = !this.openMenu
-
-          if(this.ecran <576){
-            this.openMenuSmall = !this.openMenuSmall
-          }
+      if (this.ecran < 576) {
+        this.openMenuSmall = !this.openMenuSmall;
       }
-
-      // this.isClicked = !this.isClicked;
- 
- }
-
+    }
+  }
 }
--- front-end/src/app/dashboards/dashboard-home/dashboard-home.component.ts
+++ front-end/src/app/dashboards/dashboard-home/dashboard-home.component.ts
@@ -8,51 +8,34 @@ import { ToggleService } from 'src/app/core/services/toggle/toggle.service';
 })
 export class DashboardHomeComponent { 
 
-  public openMenu : boolean = false;
-  public openMenuMobile : boolean = false;
-  public openMenuTablette : boolean = false;
-  public openMenuSmall : boolean = false;
-  public ecran : number = window.innerWidth;
-  
-  constructor(private toggleService: ToggleService){}
+  public open: boolean = false;
+  public openMenu: boolean = false;
+  public openMenuSmall: boolean = false;
+  public ecran: number = window.innerWidth;
+  public smallDevise: boolean = false;
+  public block: boolean = false;
 
- ngOnInit(): void {
-     
- }
+  //   constructor(private toggleService: ToggleService){}
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
     this.ecran = window.innerWidth;
 
-    if(this.ecran > 0 && this.ecran <=576){
-      this.openMenuSmall = false; 
+    if (this.ecran < 1010) {
+      this.smallDevise = !this.smallDevise;
     }
-    if(this.ecran > 0 && this.ecran <=576){
-      this.openMenuSmall = false; 
-    }
-   }
-
-   
-  onBars(e:MouseEvent){
-    e.preventDefault()
-    
-    if(this.ecran > 0 && this.ecran <=576)
-    {
-      this.openMenuMobile = !this.openMenuMobile
-      this.openMenuSmall = false; 
-    } 
-    else if(this.ecran > 576 && this.ecran <778)
-    {
-      this.openMenuTablette = !this.openMenuTablette; 
-      this.openMenuSmall = false; 
-    }
-    else if(this.ecran > 778)
-    {
-        this.openMenuSmall = !this.openMenuSmall;     
-    }
-    
   }
 
+  onMenu(e: MouseEvent) {
+    if (this.ecran > 1010) {
+      this.open = !this.open;
+    } else if (this.ecran < 1010) {
+      this.openMenu = !this.openMenu;
 
+      if (this.ecran < 576) {
+        this.openMenuSmall = !this.openMenuSmall;
+      }
+    }
+  }
 
 }
