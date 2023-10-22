570bfc3a3d2897ac324f30e5dcd0a6994206ea55
mnd07 <115747902+mnd07@users.noreply.github.com>
2023-10-22 13:23:39 +0200

    DA-151

---
+++ front-end/cypress/e2e/home.cy.ts
@@ -0,0 +1,11 @@
+describe('My First Test', () => {
+    it('Visits the initial project page', () => {
+      cy.visit('/')
+      cy.contains('Home')
+      cy.contains('hehhhehe')
+    })
+  })
+  
+  
+  
+  
