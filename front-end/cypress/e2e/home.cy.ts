0b9d2a2b18fd25b7ab7e7e22ffb08056ad621cdc
mnd07 <115747902+mnd07@users.noreply.github.com>
2023-11-13 14:00:22 +0100

    DA-151 "try test"

--- front-end/cypress/e2e/home.cy.ts
+++ front-end/cypress/e2e/home.cy.ts
@@ -1,8 +1,17 @@
-describe('My First Test', () => {
+describe('Test page Home', () => {
     it('Visits the initial project page', () => {
       cy.visit('/')
       cy.contains('Home')
-      cy.contains('hehhhehe')
+      cy.contains('Thés')
+      cy.contains('Thé')
+      cy.contains('TheTipTop')
+      cy.contains('types')
+      cy.contains('Jeu')
+      cy.contains('saveurs')
+      cy.contains('bio')
+      cy.contains('BIO')
+      cy.contains('noir')
+      cy.contains('vert')
     })
   })
   
