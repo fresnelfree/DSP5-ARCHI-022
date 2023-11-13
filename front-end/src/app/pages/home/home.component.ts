01f2da38928023d9eaf685e971bf0904d88823e3
mnd07 <115747902+mnd07@users.noreply.github.com>
2023-11-13 15:53:12 +0100

    DA-151 " Late update 2.1"

--- front-end/src/app/pages/home/home.component.ts
+++ front-end/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  
+
+  constructor(
+    private router : Router,
+    private titleAccueil: Title,
+    private meta: Meta
+  ) { 
+    this.titleAccueil.setTitle('Accueil');
+    this.meta.addTag({name:'Premier page', content:'Home'});
+    this.meta.addTag({name:'keywords', content:'Thé, thés bios, TheTipTop, Nice, Jeu Concours, thé vert, thé noir, infusions'});
+  }
 
 }
