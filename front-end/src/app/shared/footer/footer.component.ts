38a1443f602c70c8db88548112246959e55bfd92
Issa-barry <issabarry67@gmail.com>
2023-12-11 23:15:33 +0100

    DA-209-fix  gestion newsletter

--- front-end/src/app/shared/footer/footer.component.ts
+++ front-end/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from 'express';
+import { SnackbarService } from 'src/app/core/notification/snackbar.service';
+import { NewsletterService } from 'src/app/core/services/newsletter/newsletter.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+ 
+  constructor(
+    private fb: FormBuilder,
+    private snackbarService: SnackbarService,
+     private newsletterService: NewsletterService
+  ) {}
+
+  error_messages = {
+    'email' : [
+      // {type:'required', message:'L\'email est obligatoire.'},
+      {type: 'pattern', message: 'Adresse email invalide.' },
+    ],
+  }
+
+  newslettersForm: FormGroup = this.fb.group({
+    email: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(100),
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+      ])),
+    subscribe_date: new FormControl( new Date().toISOString())
+  })
+
+  NewsletterService():void {
+    if (this.newslettersForm.invalid) {
+      return;
+    }
+    // console.log(this.newslettersForm.value);
+    
+    this.newsletterService.subscribeNewsletter(this.newslettersForm.value.email)
+    .subscribe(
+      (res:any) => {
+        console.log("res :",res)
+        this.snackbarService.showNotification(
+          'Vous recevrez dans les prochains jour nos offres et promotions sur nos produits !',
+          'ok',
+          'success'
+        );
+      }
+    )
+  }
 
 }
