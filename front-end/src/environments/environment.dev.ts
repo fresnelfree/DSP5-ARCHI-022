cb988551eff07ba825bf5d00378b8d6cef8ab488
fresnel <56310034+fresnelfree@users.noreply.github.com>
2023-12-12 10:18:20 +0100

    Update environment.dev.ts

--- front-end/src/environments/environment.dev.ts
+++ front-end/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // hostLine: "http://thetiptop.release.api.com",
   // hostLine: "http://thetiptop.int.api.com",
-  hostLine: "https://thetiptop.life/",
+  hostLine: "https://thetiptop.life",
   // hostLine: "http://127.0.0.1:4000",
   // hostLine: "http://thetiptop.int.api.com",
   // hostLine: "http://issabarry.fr"
