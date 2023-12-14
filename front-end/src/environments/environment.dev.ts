17a9b27ab6361ae4e3c2d3b95d39e0a14c765f84
mnd07 <115747902+mnd07@users.noreply.github.com>
2023-12-14 16:23:22 +0100

    Update environment.dev.ts

--- front-end/src/environments/environment.dev.ts
+++ front-end/src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   // hostLinePPD: "http://thetiptop.release.api.com",, 
   // hostLineINT: "http://thetiptop.int.api.com", 
   // hostLinePROD: "http://issabarry.fr"
-  // hostLocal: "http://127.0.0.1:4000",
+  //// hostLocal: "http://127.0.0.1:4000",
   hostLine: "http://thetiptop.int.api.com", 
   // hostLine: "http://issabarry.fr"
 };
